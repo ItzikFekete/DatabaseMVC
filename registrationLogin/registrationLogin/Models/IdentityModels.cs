4900acf024422b0f37772da2199731f2ea93ecd6
ItzikFekete <34029678+ItzikFekete@users.noreply.github.com>
2018-03-27 16:40:50 +0100

    all from before

---
+++ registrationLogin/registrationLogin/Models/IdentityModels.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace registrationLogin.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string Code { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string fLAddress { get; set; }
+        public string sLaddress { get; set; }
+        public string city { get; set; }
+        public string postCode { get; set; }
+        public bool orderbyPhone { get; set; }
+        public int phoneNumber { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
